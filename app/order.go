bc0e0d725527ebcd102c65cd1296a295bc998adc
Richard JE Cooke <RichardJECooke@pm.me>
2023-10-16 12:46:11 +0200

    Got example code to run and return values from API

---
+++ app/order.go
@@ -0,0 +1,24 @@
+package club
+
+import (
+	order "app/gen/order"
+	"context"
+	"log"
+)
+
+// order service example implementation.
+// The example methods log the requests and return zero values.
+type ordersrvc struct {
+	logger *log.Logger
+}
+
+// NewOrder returns the order service implementation.
+func NewOrder(logger *log.Logger) order.Service {
+	return &ordersrvc{logger}
+}
+
+// Order a cup of tea.
+func (s *ordersrvc) Tea(ctx context.Context, p *order.TeaPayload) (res string, err error) {
+	s.logger.Print("order.tea")
+	return "A nice cup of tea", nil
+}
